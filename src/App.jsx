84b20f5ad5fbdbb224a723c720574c5818cb7df7
SALAB\dellv <salabyaham@gmail.com>
2024-02-05 21:32:40 +0600

    Quran_Hadith clinet side has been initialized with side par and nav bar

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Outlet } from 'react-router-dom'
+import './App.css'
+import SideNavigation from './SideNavigation/SideNavigation'
+import Heading from './Heading/Heading'
+
+function App() {
+  
+  return (
+    <>
+      <Heading></Heading>
+      <SideNavigation></SideNavigation>
+      <Outlet></Outlet>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Heading/Heading.jsx
@@ -0,0 +1,46 @@
+import { RxAvatar } from "react-icons/rx";
+
+
+const Heading = () => {
+
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </div>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                           
+
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">Dua Page</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <RxAvatar className="text-4xl"></RxAvatar>
+                            </div>
+                        </div>
+                        <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                           
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+        </div >
+    );
+};
+
+export default Heading;
