88fcfb78b6dac8c6de5559e8510f1c5fb912ca65
SALAB\dellv <salabyaham@gmail.com>
2024-02-13 22:47:32 +0600

    main category loaded from the databases, error of Public Api has been solved

--- src/Categories/MainCategory.jsx
+++ src/Categories/MainCategory.jsx
@@ -1,7 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
 import PublicApi from "../Hooks/PublicApi";
 
 const MainCategory = () => {
     const PublicApi_url = PublicApi()
+    const {isLoading,isError,error,data:mainCat=[]} = useQuery({
+        queryKey: ['category'],
+        queryFn: async ()=>{
+            const res =await PublicApi_url.get("/allcategory")
+            return res.data
+           
+        }
+
+    })
+    if(isLoading){
+        return <span>Loading</span>
+    }
+    if(isError){
+        return <span>Error : {error.message}</span>
+    }
     return (
         <div className="w-[240px] fixed min-h-screen border rounded-lg">
             <div>
@@ -15,7 +31,10 @@ const MainCategory = () => {
             </div>
             <div>
                 {/* load categories */}
-                
+                {
+                    mainCat.map((cat)=><button className="items-center justify-center" key={cat.key}>{cat.cat_name_en}</button>)
+                }
+
             </div>
             
         </div>
--- src/Hooks/PublicApi.jsx
+++ src/Hooks/PublicApi.jsx
@@ -1,8 +1,12 @@
 import axios from "axios";
 
 
-const PublicApi = axios.create({
+const url = axios.create({
     baseURL:"http://localhost:5000"
 })
+
+const PublicApi=()=>{
+    return url;
+}
 
 export default PublicApi;
